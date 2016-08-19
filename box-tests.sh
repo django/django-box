28aa93cfb82c7468ef292811505edf4e2a2fcb79
Josh Smeaton <josh.smeaton@gmail.com>
2016-08-19 23:22:33 +1000

    Fixed postgres test commands

--- box-tests.sh
+++ box-tests.sh
@@ -9,19 +9,19 @@ set -x  # To output the commands as they are run.
 runtests2.7-spatialite gis_tests
 runtests2.7-postgis gis_tests
 runtests2.7-sqlite3 auth_tests
-runtests2.7-postgresql auth_tests
+runtests2.7-postgres auth_tests
 runtests2.7-mysql auth_tests
 
 runtests3.4-spatialite gis_tests
 runtests3.4-postgis gis_tests
 runtests3.4-sqlite3 auth_tests
-runtests3.4-postgresql auth_tests
+runtests3.4-postgres auth_tests
 runtests3.4-mysql auth_tests
 
 runtests3.5-spatialite gis_tests
 runtests3.5-postgis gis_tests
 runtests3.5-sqlite3 auth_tests
-runtests3.5-postgresql auth_tests
+runtests3.5-postgres auth_tests
 runtests3.5-mysql auth_tests
 
 runtests2.7-sqlite3 admin_widgets --selenium chrome
