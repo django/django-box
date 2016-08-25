4836fefea7a7169f938763b8315ff71aa9d07252
Josh Smeaton <josh.smeaton@gmail.com>
2016-08-25 23:13:30 +1000

    Fixed test names and environments

--- box-tests.sh
+++ box-tests.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-# Smoke tests for the djangobox.
+# Smoke tests for the django-box.
 # To run this script inside the VM:
 # $ source /vagrant/box-tests.sh
 
 set -x  # To output the commands as they are run.
+date -Is
 
-runtests27-sqlite3_gis gis_tests
-runtests27-postgres_gis gis_tests
-runtests27-mysql_gis gis_tests
+runtests27-sqlite3-gis gis_tests
+runtests27-postgres-gis gis_tests
+runtests27-mysql-gis gis_tests
 runtests27-sqlite3 auth_tests
 runtests27-postgres auth_tests
 runtests27-mysql auth_tests
 
-runtests34-sqlite3_gis gis_tests
-runtests34-postgres_gis gis_tests
-runtests34-mysql_gis gis_tests
+runtests34-sqlite3-gis gis_tests
+runtests34-postgres-gis gis_tests
+runtests34-mysql-gis gis_tests
 runtests34-sqlite3 auth_tests
 runtests34-postgres auth_tests
 runtests34-mysql auth_tests
 
-runtests35-sqlite3_gis gis_tests
-runtests35-postgres_gis gis_tests
-runtests35-mysql_gis gis_tests
+runtests35-sqlite3-gis gis_tests
+runtests35-postgres-gis gis_tests
+runtests35-mysql-gis gis_tests
 runtests35-sqlite3 auth_tests
 runtests35-postgres auth_tests
 runtests35-mysql auth_tests
 
 runtests27-sqlite3 admin_widgets --selenium chrome
 runtests-flake8
+runtests-docs
+runtests-isort
+runtests-javascript
 
+date -Is
 set +x
